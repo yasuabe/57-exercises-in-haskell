0824c45a5bcde1047a459a8c0233b783d1a011ba
yasuabe2613 <yasu@yasuabe.net>
2025-05-01 17:42:29 +0900

    solve ex03

--- ex03/Main.hs
+++ ex03/Main.hs
@@ -1,4 +1,16 @@
+-- Ex3: Printing Quotes
+--
+-- - Prompt the user to enter a quote.
+-- - Prompt the user to enter the author of the quote.
+-- - Display the author and quote using escaped quotation marks.
+-- - Use string concatenation, not interpolation or substitution.
+-- - Use a single output statement for the result.
 module Main where
+import Common.Util (promptInput)
 
 main :: IO ()
-main = putStrLn "ex03"
+main = do
+  quote  <- promptInput "What is the quote? "
+  author <- promptInput "Who said it? "
+  let output = author ++ " says, \"" ++ quote ++ "\""
+  putStrLn output
