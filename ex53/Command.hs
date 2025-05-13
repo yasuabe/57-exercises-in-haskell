5483b204b802c64922f7c8541f88eb553692a884
yasuabe2613 <yasu@yasuabe.net>
2025-05-14 01:47:06 +0900

    prohibit empty `:add` and `:remove` command

--- ex53/Command.hs
+++ ex53/Command.hs
@@ -16,8 +16,10 @@ data Command
 parseText:: Text -> Maybe Command
 parseText input =
   case input' =~ pattern :: (Text, Text, Text, [Text]) of
+    (_, _, _, ["add",    ""    ]) -> Nothing
     (_, _, _, ["add",    task  ]) -> Just $ AddTask task
     (_, _, _, ["list",   _     ]) -> Just ListTasks
+    (_, _, _, ["remove", ""    ]) -> Nothing
     (_, _, _, ["remove", taskId]) -> Just $ RemoveTask taskId
     (_, _, _, ["exit",   _     ]) -> Just Exit
     _                             -> Nothing
